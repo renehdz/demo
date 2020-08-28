1f78f30e680f5f2d5abc18eea6f4f200bafb9e19
meco <meco@localhost.localdomain>
2020-08-27 23:31:00 -0600

    Adds simple endpoint with logic

--- src/main/java/com/spring/demo/controller/DemoController.java
+++ src/main/java/com/spring/demo/controller/DemoController.java
@@ -1,13 +1,19 @@
 package com.spring.demo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    @RequestMapping(value = "/")
-    public String index() {
-        return "Sample response";
+    @RequestMapping(value = "/isNumeric/{val}")
+    public String validate(@PathVariable(value = "val")String val) {
+        try {
+            Double.parseDouble(val);
+            return val + " is numeric";
+        } catch (NumberFormatException e) {
+            return val + " is not numeric";
+        }
     }
 }
